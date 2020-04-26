fa6df530eea3863257a4560ca5f90ae8c44e7bbd
stefaneutu <62829426+stefaneutu@users.noreply.github.com>
2020-04-26 11:35:24 +0300

    Update __init__.py

--- linepy/__init__.py
+++ linepy/__init__.py
@@ -4,11 +4,11 @@ from .channel import Channel
 from .oepoll import OEPoll
 from akad.ttypes import OpType
 
-__copyright__       = 'Copyright 2018 by Fadhiil Rachman'
+__copyright__       = 'Copyright 2020 by Blue Eyes'
 __version__         = '3.0.8'
 __license__         = 'BSD-3-Clause'
 __author__          = 'Fadhiil Rachman'
-__author_email__    = 'fadhiilrachman@gmail.com'
-__url__             = 'http://github.com/fadhiilrachman/line-py'
+__author_email__    = 'stefandincamail@gmail.com'
+__url__             = 'http://github.com/stefaneutu/line-py'
 
 __all__ = ['LINE', 'Channel', 'OEPoll', 'OpType', 'Call']
